baa111a39958b6f6c680ac433ffbbd7699794646
aadiavlani <aadia2026@student.cis.edu.hk>
2024-01-31 10:23:08 +0800

    Ms. Nahar, my first lab is uploaded.

--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 public class Pages {
 
     public static void main(String[] args) {
-
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter Your Age");
+    int age = scan.nextInt();
+    int minpagestoread = (100 - age);
+    System.out.println("The minimum number of pages you need to read before giving up on a book is: " + minpagestoread);
     }
 }
